35db2c36fef0ca8b57379d9c8d40295982cb47bd
mbuthia wanjiru <sheezwagichuru.gmail.com>
2024-03-20 09:14:27 +0300

    controlflow

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance  <= 400)
+  {return 'This one is on me!';}
+  else if  ( distance <=2000)
+  {return 'That will be twenty bucks.';}
+  else if (distance<=2500)
+  {return 'I will gladly take your thirty bucks.';}
+  else 
+  {return 'No can do.';}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destination){
   // Write your code here!
-}
+  return destination === "NYC" ? "Ok, sounds good.":"No go.";}
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
+  if (tipAmount === 'generous')
+  { return 'Thank you so much.';}
+  else if (tipAmount === 'not as generous')
+  {return 'Thank you.';}
+  else
+  {return 'Bye.';}
 }
